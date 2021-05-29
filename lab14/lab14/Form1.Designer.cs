4f33458c21a7c6e89307a5b68cbb74db209da6f6
ParadiZzzze <73051260+ParadiZzzze@users.noreply.github.com>
2021-05-29 16:34:28 +0700

    Add files via upload

---
+++ lab14/lab14/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace lab14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.ed_ExpectedValue = new System.Windows.Forms.TextBox();
+            this.ed_Dispersion = new System.Windows.Forms.TextBox();
+            this.ed_NumberOfExperiments = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ed_ExpectedValue
+            // 
+            this.ed_ExpectedValue.Location = new System.Drawing.Point(11, 11);
+            this.ed_ExpectedValue.Margin = new System.Windows.Forms.Padding(2);
+            this.ed_ExpectedValue.Name = "ed_ExpectedValue";
+            this.ed_ExpectedValue.Size = new System.Drawing.Size(76, 20);
+            this.ed_ExpectedValue.TabIndex = 0;
+            this.ed_ExpectedValue.Text = "5";
+            // 
+            // ed_Dispersion
+            // 
+            this.ed_Dispersion.Location = new System.Drawing.Point(11, 57);
+            this.ed_Dispersion.Margin = new System.Windows.Forms.Padding(2);
+            this.ed_Dispersion.Name = "ed_Dispersion";
+            this.ed_Dispersion.Size = new System.Drawing.Size(76, 20);
+            this.ed_Dispersion.TabIndex = 1;
+            this.ed_Dispersion.Text = "0,1";
+            // 
+            // ed_NumberOfExperiments
+            // 
+            this.ed_NumberOfExperiments.Location = new System.Drawing.Point(11, 103);
+            this.ed_NumberOfExperiments.Margin = new System.Windows.Forms.Padding(2);
+            this.ed_NumberOfExperiments.Name = "ed_NumberOfExperiments";
+            this.ed_NumberOfExperiments.Size = new System.Drawing.Size(76, 20);
+            this.ed_NumberOfExperiments.TabIndex = 3;
+            this.ed_NumberOfExperiments.Text = "1000";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(113, 14);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "ExpectedValue";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(113, 60);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Dispersion";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(113, 106);
+            this.label19.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(112, 13);
+            this.label19.TabIndex = 22;
+            this.label19.Text = "NumberOfExperiments";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 151);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 23);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(579, 22);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(477, 357);
+            this.chart1.TabIndex = 28;
+            this.chart1.Text = "NormalDistribution";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1076, 535);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.ed_NumberOfExperiments);
+            this.Controls.Add(this.ed_Dispersion);
+            this.Controls.Add(this.ed_ExpectedValue);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ed_ExpectedValue;
+        private System.Windows.Forms.TextBox ed_Dispersion;
+        private System.Windows.Forms.TextBox ed_NumberOfExperiments;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ lab14/lab14/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab14
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public double[] data_check()
+        {
+            double[] mean_var = new double[2];
+            mean_var[0] = Convert.ToDouble(ed_ExpectedValue.Text);
+            mean_var[1] = Convert.ToDouble(ed_Dispersion.Text);
+            return mean_var;
+        }
+
+        public double calc_ExpectedValue(List<float> values)
+        {
+            double EV = 0;
+            for (int i = 0; i < values.Count; i++)
+            {
+                EV += values[i];
+            }
+            return EV/values.Count;
+        }
+        public double calc_Dispersion(List<float> values, double mean)
+        {
+            double E = mean;
+            double Dispersion = 0;
+            for (int i = 0; i < values.Count; i++)
+            {
+                Dispersion += values[i]* values[i];
+            }
+            Dispersion /= values.Count;
+            Dispersion -= E * E;
+            return Dispersion;
+        }
+        private int Check(List<float> values, double a, double b)
+        {
+            return values.Count(t => t >= a && t < b);
+        }
+        public double calc_statistics(List<float> values, double mean, double d)
+        {
+            chart1.Series.Clear();
+            Series series = new Series();
+            series.ChartType = SeriesChartType.Column;
+            series.Name = "Distribution";
+            chart1.Series.Add(series);
+            double max = values.Max();
+            double min = values.Min();
+            double K = (max - min) * 0.05;
+            double interval = (max - min) + K;
+
+            int k = (int)Math.Round(Math.Log(values.Count) / Math.Log(2) + 1);
+            double step = interval / k;
+
+            double q = min - K / 2;
+            int[] stat = new int[k];
+            float chi = 0;
+
+            for (int i = 0; i < k; i++)
+            {
+                stat[i] = Check(values,q, q + step);
+                float p = (float)(q + step - q) * Density((float)(q + step + q) / 2, mean, d);
+                chi += (float)Math.Pow(stat[i], 2) / (values.Count * p);
+                chart1.Series[0].Points.AddXY(q, (double)stat[i] / values.Count);
+                q += step;
+            }
+
+            chi -= values.Count;
+            return chi;
+        }
+        private float Density(float x, double mean, double d)
+        {
+            var value = (float)Math.Pow(Math.E, Math.Pow((x - mean), 2) / (-2 * Math.Pow(d, 2)));
+            return value / (float)(d * Math.Sqrt(2 * Math.PI));
+        }
+        public List<float> calculation(double[] mean_var, int number_experiment)
+        {
+            Random rnd = new Random();
+            List<float> values = new List<float>();
+            for (int i = 0; i < number_experiment; i++)
+            {
+                double a1 = rnd.NextDouble();
+                double a2 = rnd.NextDouble();
+                double kor = Math.Sqrt(-2 * Math.Log(a1));
+                double cos = Math.Cos(2 * Math.PI * a2);
+                float x = (float)(kor * cos * Math.Sqrt(mean_var[1]) + mean_var[0]);
+                values.Add(x);
+            }
+            return values;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double[] mean_var = data_check();
+            int sample_size;
+            sample_size = Convert.ToInt32(ed_NumberOfExperiments.Text);
+  
+            List<float> samples = calculation(mean_var, sample_size);
+            double mean = Math.Round(calc_ExpectedValue(samples),2);
+            double deviation1 = Math.Round(Math.Abs(mean - mean_var[0]) / Math.Abs(mean) * 100);
+            label7.Text = Convert.ToString($"Average: {mean}, deviation: {deviation1}%" );
+            double d = calc_Dispersion(samples, mean);
+            double deviation2 = Math.Round(Math.Abs(d - mean_var[1]) / Math.Abs(mean_var[1]) * 100, 2);
+            label8.Text = Convert.ToString($"Dispersion: {d}, deviation: {deviation2}%");
+      
+            double chi = calc_statistics(samples, mean, d);
+            label19.Text = "Chi = " + Convert.ToString(chi);
+        }
+
+    }
+}
